ebd003be0976fd73826c29f99eca63e2ddcbf543
jzamora <jzamora@miteleferico.bo>
2024-07-09 13:58:03 -0400

    feeling whhel

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:disenos_app/src/pages/headers_page.dart';
+import 'package:disenos_app/src/pages/feeling_wheel_page.dart';
+// import 'package:disenos_app/src/pages/headers_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HeadersPage(),
+      home: FeelingWheelPage(),
     );
   }
 }
